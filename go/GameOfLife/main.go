a17da2d8812b8779d2a93afca6249d4fe46d0bda
Axel <axel.coronel@mercadolibre.com>
2017-09-11 09:48:04 -0300

    prints

--- go/GameOfLife/main.go
+++ go/GameOfLife/main.go
@@ -25,28 +25,32 @@ func printAndWait(board string) {
 }
 
 func main() {
-	defer board.Un(board.Trace("main"))
+
 	h := flag.Int("h", 10, "Largo del tablero")
 	w := flag.Int("w", 30, "Ancho del tablero")
 	p := flag.Int("p", 25, "Propabilidad de que una celda este viva al inicio")
+	t := flag.Int("t", 600, "Cantidad de ciclos")
 	r := flag.Bool("r", false, "Mostrar paso a paso la evolucion del tablero")
-
 	flag.Parse()
 
 	render := *r
+	n := *t
+
+	if !render {
+		defer board.Un(board.Trace("main"))
+	}
 
 	b := board.Board{}
-
 	str := b.Init(*h, *w, *p, render)
 	if render {
 		clear()
-		fmt.Println("0%")
+		fmt.Printf("0%% (0/%d)\n", n)
 		printAndWait(str)
 	}
 
 	clear()
-	for i := 0; i < 600; i++ {
-		fmt.Printf("%d%%\n", (i*100)/600)
+	for i := 0; i < n; i++ {
+		fmt.Printf("%d%% (%d/%d)\n", (((i + 1) * 100) / n), i+1, n)
 		str := b.Next(render)
 		if render {
 			printAndWait(str)
