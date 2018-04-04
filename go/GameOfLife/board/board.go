797bdcfe843aaccf2a88861d701e0e12e416f8fb
Axel <axel.coronel@mercadolibre.com>
2018-04-04 19:28:16 -0300

    v2

--- go/GameOfLife/board/board.go
+++ go/GameOfLife/board/board.go
@@ -2,54 +2,258 @@ package board
 
 import (
 	"bytes"
+	"fmt"
 	"math/rand"
-	"strconv"
+	"sync"
+)
+
+const (
+	_defaultWorkerSize = 10
+
+	_alive = true
+	_dead  = false
+
+	_actionSearch   = 1
+	_actionNoSearch = 0
+
+	_survives   = 1
+	_noSurvives = 0
 )
 
 type Board struct {
-	cells map[string]*Cell
-	h, w  int
+	cells map[[2]int]bool
+
+	aliveCells chan [2]int
+
+	render bool
+
+	h int
+	w int
+
+	mutex sync.RWMutex
 }
 
-func (b *Board) Init(w, h, prob int, render bool) string {
+func (b *Board) Init(h, w, prob int, render bool) string {
 
-	b.w = w
 	b.h = h
-	b.cells = make(map[string]*Cell)
+	b.w = w
+	b.render = render
+
+	b.cells = make(map[[2]int]bool, w*h)
+	b.aliveCells = make(chan [2]int, w*h)
 
 	for x := 0; x < w; x++ {
 		for y := 0; y < h; y++ {
-			alive := rand.Intn(101) < prob
-			cell := Cell{state: alive, x: x, y: y}
-			cell.Board(b)
+
+			cell := [2]int{x, y}
+			state := (rand.Intn(101) < prob)
+
+			b.cells[cell] = state
+			if state == _alive {
+				b.aliveCells <- cell
+			}
 		}
 	}
 
-	if render {
+	if b.render {
 		return b.String()
 	}
+
 	return ""
 }
 
-func (b Board) Next(render bool) string {
-	for x := 0; x < b.w; x++ {
-		for y := 0; y < b.h; y++ {
-			cell := b.cell(x, y)
-			cell.Next()
-			defer cell.Apply()
+func (b *Board) workerFindNeighbors(cells chan [3]int, neighbors chan [2]int, results chan<- [3]int, wg *sync.WaitGroup) {
+
+	var (
+		x          int
+		y          int
+		aliveCount int
+
+		isAlive bool
+
+		cell     [2]int
+		neighbor [2]int
+
+		action int
+	)
+
+	for {
+		data, ok := <-cells
+		if !ok {
+			fmt.Printf("workerFindNeighbors - finalizando\n")
+			return
+		}
+
+		fmt.Printf("workerFindNeighbors - procesando:%v\n", cell)
+
+		x, y, action, aliveCount = data[0], data[1], data[2], 0
+
+		cell = [2]int{x, y}
+
+		for _x := x - 1; _x <= x+1; _x++ { // desplazamiento horizontal de -1,0,1 respecto a x
+
+			if _x < 0 || _x >= b.w {
+				continue
+			}
+
+			for _y := y - 1; _y <= y+1; _y++ { // desplazamiento horizontal de -1,0,1 respecto a y
+
+				if _y < 0 || _y >= b.h {
+					continue
+				}
+
+				neighbor = [2]int{_x, _y}
+
+				b.mutex.RLock()
+				isAlive = b.cells[neighbor]
+				b.mutex.RUnlock()
+
+				if isAlive {
+					fmt.Printf("workerFindNeighbors - celda:%v - vecino:%v\n", cell, neighbor)
+					aliveCount = aliveCount + 1
+				}
+
+				if action == _actionSearch {
+					fmt.Printf("workerFindNeighbors - %v notifica a :%v | cells:%v\n", cell, neighbor, len(cells))
+					neighbors <- neighbor
+				}
+
+			}
+		}
+
+		nextStatus := _noSurvives
+		if aliveCount == 3 { // B3?
+			nextStatus = _survives
+		} else {
+
+			b.mutex.RLock()
+			currentStatus := b.cells[cell]
+			b.mutex.RUnlock()
+
+			if currentStatus && aliveCount == 2 { // S2?
+				nextStatus = _survives
+			}
+		}
+
+		fmt.Printf("workerFindNeighbors - resultado para %v: %v | results:%v\n", cell, nextStatus, len(results))
+
+		results <- [3]int{x, y, nextStatus}
+
+		wg.Done()
+
+	}
+}
+
+func (b *Board) Next() string {
+
+	// recorro las vivas
+	// cuento cuantos vecinos vivos tiene
+	// verifico rule y notifico si queda viva o no
+	// por cada vecino
+	// cuanto cuentos vecinos vivos tiene
+	// verifico rule y notifico si queda viva o no
+
+	// wait
+
+	// marco cada celda viva o muerta como tal
+	// mantengo las celdas vivas
+
+	wg := sync.WaitGroup{}
+	totalWork := len(b.aliveCells) * 9 // celdas vivas + vecinas
+
+	if totalWork > b.h*b.w { // nunca puede haber mas trabajo que celdas
+		totalWork = b.h * b.w
+	}
+
+	cells := make(chan [3]int, totalWork)
+	neighbors := make(chan [2]int, totalWork)
+
+	results := make(chan [3]int, totalWork)
+
+	aliveCells := make(chan [2]int, totalWork)
+	deadCells := make(chan [2]int, totalWork)
+
+	fmt.Printf("Next - totalWork:%v\n", totalWork)
+
+	wg.Add(totalWork)
+
+	// de todas las celdas, las unicas que me interesa validar son:
+	// celdas vivas y sus vecinas
+
+	go func() {
+		for i := 0; i < _defaultWorkerSize; i++ {
+			go b.workerFindNeighbors(cells, neighbors, results, &wg)
+			// se crean aca, pero no se usan hasta el final
+			go b.workerSetAlive(aliveCells, &wg)
+			go b.workerSetDead(deadCells, &wg)
+		}
+	}()
+
+	processedCells := make(map[[2]int]bool, totalWork)
+	m := sync.RWMutex{}
+
+	go func() { // mando cada celda viva a los workers para que busquen los vecinos
+		for cell := range b.aliveCells {
+			fmt.Printf("Next - mando a buscar vecinos para [%v %v] | b.aliveCells:%v\n", cell[0], cell[1], len(b.aliveCells))
+			m.Lock()
+			processedCells[cell] = true
+			m.Unlock()
+			cells <- [3]int{cell[0], cell[1], _actionSearch}
+		}
+	}()
+
+	go func() { // mando cada celda viva a los workers para que busquen los vecinos
+		for neighbor := range neighbors {
+			m.RLock()
+			processed := processedCells[neighbor]
+			m.RUnlock()
+			if !processed {
+				fmt.Printf("Neighbors - mando a procesar %v\n", neighbor)
+				m.Lock()
+				processedCells[neighbor] = true
+				m.Unlock()
+				cells <- [3]int{neighbor[0], neighbor[1], _actionNoSearch}
+			} else {
+				fmt.Printf("Neighbors - %v ya esta procesado\n", neighbor)
+			}
+		}
+	}()
+
+	wg.Wait()
+	close(cells)
+	close(results)
+	// close(neighbors)
+
+	fmt.Printf("Next - fase 2 | results:%v\n", len(results))
+
+	b.aliveCells = make(chan [2]int, b.w*b.h) // en el unico momento en que deberia estar abierto este canal
+	wg.Add(totalWork)
+
+	for result := range results {
+
+		cell := [2]int{result[0], result[1]}
+		nextStatus := result[2]
+
+		switch nextStatus {
+		case _survives:
+			aliveCells <- cell
+		default: // no survive
+			deadCells <- cell
 		}
 	}
 
-	if render {
+	wg.Wait()
+
+	close(aliveCells)
+	close(deadCells)
+	close(b.aliveCells) // siempre deberia estar cerrado
+
+	if b.render {
 		return b.String()
 	}
+
+	// cuando
 	return ""
-
-}
-
-func (b Board) cell(x, y int) *Cell {
-	point := strconv.Itoa(x) + strconv.Itoa(y)
-	return b.cells[point]
 }
 
 func (b Board) String() string {
@@ -57,14 +261,154 @@ func (b Board) String() string {
 
 	for x := 0; x < b.w; x++ {
 		for y := 0; y < b.h; y++ {
-			point := strconv.Itoa(x) + strconv.Itoa(y)
-			cell := b.cells[point]
-			if cell != nil {
-				b := cell.Row()
-				buf.WriteByte(b)
+
+			if b.cells[[2]int{x, y}] { // alive?
+				buf.WriteByte('*')
+			} else {
+				buf.WriteByte(' ')
 			}
 		}
 		buf.WriteByte('\n')
 	}
 	return buf.String()
+}
+
+// workers
+
+func (b *Board) workerSearchTargetCells(cells <-chan [2]int, neighbors chan<- [2]int, wg *sync.WaitGroup) {
+
+	notified := make(map[[2]int]bool, b.w*b.h)
+	mutex := sync.Mutex{}
+	for {
+		cell, ok := <-cells
+		if !ok {
+			return
+		}
+
+		x, y := cell[0], cell[1]
+
+		for _x := x - 1; _x <= x+1; _x++ { // desplazamiento horizontal de -1,0,1 respecto a x
+
+			if _x < 0 || _x > b.w {
+				continue
+			}
+
+			for _y := y - 1; _y <= y+1; _y++ { // desplazamiento horizontal de -1,0,1 respecto a y
+
+				if _y < 0 || _y > b.h {
+					continue
+				}
+
+				c := [2]int{_x, _y}
+
+				mutex.Lock()
+				_, processed := notified[cell]
+				if processed {
+					mutex.Unlock()
+					continue
+				}
+
+				notified[cell] = true
+				mutex.Unlock()
+
+				// incluyo la celda (x,y) adrede
+				neighbors <- c
+			}
+		}
+
+		wg.Done()
+
+	}
+
+}
+
+func (b *Board) workerCheckCells(cells <-chan [2]int, aliveCells chan<- [2]int, deadCells chan<- [2]int, wg *sync.WaitGroup) {
+
+	// validar si la celda esta viva y si cumple con las reglas
+
+	// marcar la celda en b.cells como viva / muerta
+
+	// si esta viva, mandarla a aliveCells, sino, a deadCells
+
+	for {
+		cell, ok := <-cells
+		if !ok {
+			return
+		}
+
+		x, y, alive := cell[0], cell[1], 0
+
+		for _x := x - 1; _x <= x+1; _x++ { // desplazamiento horizontal de -1,0,1 respecto a x
+
+			if _x < 0 || _x > b.w { // _x esta fuera de los limites
+				continue
+			}
+
+			for _y := y - 1; _y <= y+1; _y++ { // desplazamiento horizontal de -1,0,1 respecto a y
+
+				if _y < 0 || _y > b.h || (_x == x && _y == y) {
+					// _x esta fuera de los limites
+					// (_x,_y) == (x,y) es la celda a evaluar, por lo que la ignoro
+					continue
+				}
+
+				neighbor := [2]int{_x, _y}
+				b.mutex.RLock()
+				neighborAlive := b.cells[neighbor]
+				b.mutex.RUnlock()
+
+				if neighborAlive { // la celda vecina esta viva?
+					alive = alive + 1
+				}
+
+			}
+		}
+
+		// validar regla (B3/S2)
+
+		if alive == 3 { // B3?
+			aliveCells <- cell
+		} else {
+
+			b.mutex.RLock()
+			c := b.cells[cell]
+			b.mutex.RUnlock()
+
+			if c && alive == 2 { // S2?
+				aliveCells <- cell
+			} else {
+				deadCells <- cell
+			}
+		}
+
+		wg.Done()
+
+	}
+}
+
+func (b *Board) workerSetAlive(cells <-chan [2]int, wg *sync.WaitGroup) {
+	for { // marco como viva + agrego al listado de celdas vivas para la prox ronda
+		cell, ok := <-cells
+		if !ok {
+			return
+		}
+		b.mutex.Lock()
+		b.cells[cell] = true
+		b.mutex.Unlock()
+		b.aliveCells <- cell
+		wg.Done()
+	}
+}
+
+func (b *Board) workerSetDead(cells <-chan [2]int, wg *sync.WaitGroup) {
+	for { // marco como viva + agrego al listado de celdas vivas para la prox ronda
+		cell, ok := <-cells
+		if !ok {
+			return
+		}
+		b.mutex.Lock()
+		b.cells[cell] = false
+		b.mutex.Unlock()
+		wg.Done()
+	}
 }
--- go/GameOfLife/main.go
+++ go/GameOfLife/main.go
@@ -4,12 +4,9 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"math/rand"
 	"os"
 	"os/exec"
-	"runtime"
-	"runtime/pprof"
 	"time"
 
 	"github.com/axcoro/tesis/go/GameOfLife/board"
@@ -25,52 +22,34 @@ var reader = bufio.NewReader(os.Stdin)
 
 func printAndWait(board string) {
 	fmt.Print(board)
-	// time.Sleep(200 * time.Millisecond)
-	reader.ReadString('\n')
+	time.Sleep(200 * time.Millisecond)
+	// reader.ReadString('\n')
 }
 
 var (
-	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
-	memprofile = flag.String("memprofile", "", "write memory profile to `file`")
-	h          = flag.Int("h", 10, "Largo del tablero")
-	w          = flag.Int("w", 30, "Ancho del tablero")
-	p          = flag.Int("p", 25, "Propabilidad de que una celda este viva al inicio")
-	t          = flag.Int("t", 600, "Cantidad de ciclos")
-	s          = flag.Int64("s", 0, "Semilla para rnd")
-	r          = flag.Bool("r", false, "Mostrar paso a paso la evolucion del tablero")
+	h = flag.Int("h", 10, "Largo del tablero")
+	w = flag.Int("w", 30, "Ancho del tablero")
+	p = flag.Int("p", 25, "Propabilidad de que una celda este viva al inicio")
+	t = flag.Int("t", 600, "Cantidad de ciclos")
+	s = flag.Int64("s", 0, "Semilla para rnd")
+	r = flag.Bool("r", false, "Mostrar paso a paso la evolucion del tablero")
 )
 
 func main() {
+
 	flag.Parse()
 
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
-
-	render := *r
 	n := *t
-
-	if !render {
-		defer board.Un(board.Trace("main"))
-	}
-
 	seed := *s
+
 	if seed == 0 { // sino se define una semilla definir algo pseudo rnd
 		seed = time.Now().UTC().UnixNano()
 	}
 	rand.Seed(seed)
 
 	b := board.Board{}
-	str := b.Init(*h, *w, *p, render)
-
-	if render {
+	str := b.Init(*h, *w, *p, *r)
+	if str != "" {
 		clear()
 		fmt.Printf("0%% (0/%d)\n", n)
 		printAndWait(str)
@@ -79,22 +58,10 @@ func main() {
 	clear()
 	for i := 0; i < n; i++ {
 		fmt.Printf("%d%% (%d/%d)\n", (((i + 1) * 100) / n), i+1, n)
-		str := b.Next(render)
-		if render {
+		str := b.Next()
+		if str != "" {
 			printAndWait(str)
 		}
 		clear()
-	}
-
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
-		}
-		runtime.GC() // get up-to-date statistics
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
-		}
-		f.Close()
 	}
 }
